ea459ee63cffb7ab4df608b1030fd7f0252f92f4
Ernest Arredondo <earredondo@nexusgeographics.com>
2021-01-09 22:47:48 +0100

    Add django contrib postgres

--- django_base/settings.py
+++ django_base/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
+    'django.contrib.postgres',
     'leaflet',
     'djgeojson',
     'simple_history',
